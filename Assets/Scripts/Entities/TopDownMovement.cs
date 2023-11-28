8a42c9071bc893f31d370c94ce0d15adfa0314f9
scotcy <kimjh470@naver.com>
2023-11-28 17:03:13 +0900

    불필요한 cs삭제&통합
    마우스 방향으로 캐릭터가 보는 방향을 설정했지만 movement.cs안에 통합 시킴

--- Assets/Scripts/Entities/TopDownMovement.cs
+++ Assets/Scripts/Entities/TopDownMovement.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Claims;
 using UnityEngine;
 
 public class TopDownMovement : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer watchRenderer;//캐릭터 바라보는 시점에 따라 방향바뀌기 구현
+
     private TopDownCharacterController _controller;
 
 
@@ -20,6 +23,7 @@ public class TopDownMovement : MonoBehaviour
     private void Start()
     {
         _controller.OnMoveEvent += Move;
+        _controller.OnLookEvent += OnAim;
     }
 
     private void FixedUpdate()
@@ -36,5 +40,22 @@ public class TopDownMovement : MonoBehaviour
     {
         direction = direction * 5;
         _rigidbody.velocity = direction;
+    }
+
+    //여기서부터 캐릭터 시점 바라보는 메서드 구현
+    public void OnAim(Vector2 newWatchDirection)
+    {
+        RotateWatching(newWatchDirection);
+    }
+
+    private void RotateWatching(Vector2 direction)
+    {
+        //바라보는 방향의 벡터에서 각도 구하는 코드//Atan는 아크탄젠트로 라디안 값이 나온다.
+        float rotz = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;//라디안 각도를 오일러 각도로 변환
+
+        //각도를 계산에서 90도 이상이면 방향을 바꿔주는 코드.
+        watchRenderer.flipX = Mathf.Abs(rotz) > 90f;
+        //실제 무기 회전 시키기.
+        //armPivot.rotation = Quaternion.Euler(0, 0, rotz);//rotz는 오일러 각도(위에서 라디안을 오일러로 바꿈)
     }
 }
